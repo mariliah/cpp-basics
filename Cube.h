ca28b9ef0723aa674651bbd1630420c51fe743a1
Marilia <ramonap2013@gmail.com>
2024-02-18 20:23:01 -0600

    created cube class, main.cpp, cube.cpp makefile

---
+++ Cube.h
@@ -0,0 +1,16 @@
+// header file create interface to the class. defined separately from implementation (.cpp file)
+// declaration of all member variables AND functions
+// akin to an api
+
+#pragma once // always present in h files, compiler will run this only ONCE
+
+class Cube
+{
+public:
+    double getVolume();
+    double getSurfaceArea();
+    void setLength(double length);
+
+private:
+    double length_;
+};
